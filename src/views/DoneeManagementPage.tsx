b5de8bef8d58be559db74a86d49c59d04d54a9f9
Wuttisak02 <156461214+Wuttisak02@users.noreply.github.com>
2024-02-27 12:09:18 +0700

    first commit

--- src/views/DoneeManagementPage.tsx
+++ src/views/DoneeManagementPage.tsx
@@ -2,54 +2,52 @@ import { useAppDispatch } from "../redux/RouteStore";
 import PageTitle from "../components/PageTitle";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import Typography from '@mui/material/Typography';
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import Typography from "@mui/material/Typography";
 import { useRef, useState } from "react";
 import DoneeOnboardingModal from "../components/DoneeOnboardingModal";
+import TableBasic from "../components/List/TableBasic";
 
 export default function DoneeManagementPage() {
-    const [doneeOnboardingModalShown, setDoneeOnboardingModalShown] = useState(false);
-    const doneeOnboardingButtonRef = useRef<HTMLButtonElement>(null);
-    const dispatch = useAppDispatch();
+  const [doneeOnboardingModalShown, setDoneeOnboardingModalShown] =
+    useState(false);
+  const doneeOnboardingButtonRef = useRef<HTMLButtonElement>(null);
+  const dispatch = useAppDispatch();
 
-    const showDoneeOnboardingModal = () => {
-        setDoneeOnboardingModalShown(true);
-    };
+  const showDoneeOnboardingModal = () => {
+    setDoneeOnboardingModalShown(true);
+  };
 
-    const closeDoneeOnboardingModal = () => {
-        setDoneeOnboardingModalShown(false);
-    };
+  const closeDoneeOnboardingModal = () => {
+    setDoneeOnboardingModalShown(false);
+  };
 
-    return (
-        <>
-            <PageTitle>Donee List</PageTitle>
-            <Box
-                component="section"
-                display="flex"
-                width="100%"
-            >
-                <Box sx={{ border: '1px dashed grey' }}>
-                    <FilterAltIcon />
-                    <Typography variant="caption">Filter</Typography>
-                </Box>
-                <Box sx={{ border: '1px dashed grey' }}>
-                    <Button
-                        style={{ textTransform: 'none' }}
-                        variant="outlined"
-                        ref={doneeOnboardingButtonRef}
-                        onClick={showDoneeOnboardingModal}
-                    >
-                        Create new Donee
-                    </Button>
-                </Box>
-            </Box>
-            
-            <DoneeOnboardingModal
-                isOpen={doneeOnboardingModalShown}
-                closeModal={closeDoneeOnboardingModal}
-                onSubmit={undefined}
-            />
-
-        </>
-    );
+  return (
+    <>
+      <PageTitle>Donee List</PageTitle>
+      <Box component="section" display="flex" width="100%">
+        <Box sx={{ border: "1px dashed grey" }}>
+          <FilterAltIcon />
+          <Typography variant="caption">Filter</Typography>
+        </Box>
+        <Box sx={{ border: "1px dashed grey" }}>
+          <Button
+            style={{ textTransform: "none" }}
+            variant="outlined"
+            ref={doneeOnboardingButtonRef}
+            onClick={showDoneeOnboardingModal}
+          >
+            Create new Donee
+          </Button>
+        </Box>
+      </Box>
+      <br />
+      <TableBasic />
+      <DoneeOnboardingModal
+        isOpen={doneeOnboardingModalShown}
+        closeModal={closeDoneeOnboardingModal}
+        onSubmit={undefined}
+      />
+    </>
+  );
 }
